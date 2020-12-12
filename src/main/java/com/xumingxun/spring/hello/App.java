1c1eae21985e3526c170caaaaf69dd4cec7aec69
xmxkkk <908581016@qq.com>
2020-12-12 20:08:48 +0800

    Initial commit

---
+++ src/main/java/com/xumingxun/spring/hello/App.java
@@ -0,0 +1,18 @@
+package com.xumingxun.spring.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+    @RequestMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
